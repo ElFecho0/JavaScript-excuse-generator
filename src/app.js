47992e4f28d43758db618b847875e100533b61ff
ElFecho0 <fflores.monsivais@gmail.com>
2021-10-13 00:40:18 +0000

    Trabajo hecho

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let quien = Math.floor(Math.random() * who.length);
+  let accion = Math.floor(Math.random() * action.length);
+  let que = Math.floor(Math.random() * what.length);
+  let cuando = Math.floor(Math.random() * when.length);
+  let excusa =
+    who[quien] + " " + action[accion] + " " + what[que] + " " + when[cuando];
+
+  console.log(excusa);
+
+  document.getElementById("excuse").innerHTML = excusa;
 };
